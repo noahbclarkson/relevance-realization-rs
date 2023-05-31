15b21a3659f7e8acfe2910fe7caf2231bbb684db
Noah Clarkson <unprotesting.email@gmail.com>
2023-05-31 16:45:46 +1200

    Update mod.rs

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,6 +1,7 @@
-pub mod agent_plugin;
+pub mod agent_creation_plugin;
 pub mod app_state_plugin;
 pub mod camera_plugin;
 pub mod new_world_plugin;
 pub mod performance_plugin;
 pub mod tilemap_plugin;
+pub mod hover_plugin;
