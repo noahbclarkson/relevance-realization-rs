155205ced45b973e78d8f254ec95e9b7309f7fb7
Noah Clarkson <unprotesting.email@gmail.com>
2023-06-01 09:07:32 +1200

    Move exploit to separate function

--- src/plugins/agent_behaviour_plugin.rs
+++ src/plugins/agent_behaviour_plugin.rs
@@ -27,32 +27,15 @@ fn calculate_agent_tasks(
             // Task frequency will be a number between 0 and 1 indicating how often the agent has executed the last task (weighted by recency).
             let lowest = agent.data.lowest() / 100.0;
             let lowest_negative = 1.0 - lowest;
-            // Essentially, we want to explore if the agent is in a good state and we have repeated the same task a lot
-            // We want to exploit if the agent is in a bad state and we have not repeated the same task a lot
+            // Essentially, we want to explore if the agent is in a good state
+            // We want to exploit if the agent is in a bad state
             // We can average the two values to get a value between 0 and 1
             let exploration = lowest * agent.exploration_exploitation.lhs_multiplier();
             let exploitation = lowest_negative * agent.exploration_exploitation.rhs_multiplier();
             if exploration > exploitation {
                 explore(&mut agent, &time, &tiles, &mut agent_transform, &mut rng);
             } else {
-                let lowest_value_task_type = agent.data.get_lowest_value_task();
-                // See if we have this task type in our task history, if we do, we can move towards it
-                // If we don't we'll have to explore more
-                let a = agent.clone();
-                let task = a.find_latest_matching_task(lowest_value_task_type);
-                if let Some(task) = task {
-                    agent.agent_move(&time, task, &mut agent_transform);
-                    let mut task = task.clone();
-                    if agent.check_if_at_postion(&agent_transform, task.position()) {
-                        task.set_completed(true);
-                    }
-                    agent.exploit(&tiles, &mut agent_transform);
-                    agent.exploration_exploitation_state =
-                        ExploringExploitingState::Exploiting;
-                    agent.latest_task = lowest_value_task_type;
-                } else {
-                    explore(&mut agent, &time, &tiles, &mut agent_transform, &mut rng);
-                }
+                exploit(&mut agent, &time, &tiles, &mut agent_transform, &mut rng)
             }
         }
     }
@@ -90,7 +73,26 @@ fn explore(
     // Then we want to exploit the current tile we are on
     agent.exploit(&tiles, &mut agent_transform);
     agent.exploration_exploitation_state = ExploringExploitingState::Exploring;
+}
 
+fn exploit(agent: &mut Agent, time: &Res<Time>, tiles: &Res<Tiles>, mut agent_transform: &mut Transform, mut rng: &mut ThreadRng) {
+    let lowest_value_task_type = agent.data.get_lowest_value_task();
+    // See if we have this task type in our task history, if we do, we can move towards it
+    // If we don't we'll have to explore more
+    let a = agent.clone();
+    let task = a.find_latest_matching_task(lowest_value_task_type);
+    if let Some(task) = task {
+        agent.agent_move(&time, task, &mut agent_transform);
+        let mut task = task.clone();
+        if agent.check_if_at_postion(&agent_transform, task.position()) {
+            task.set_completed(true);
+        }
+        agent.exploit(&tiles, &mut agent_transform);
+        agent.exploration_exploitation_state = ExploringExploitingState::Exploiting;
+        agent.latest_task = lowest_value_task_type;
+    } else {
+        explore(agent, &time, &tiles, &mut agent_transform, &mut rng);
+    }
 }
 
 fn tick(mut agent_query: Query<&mut Agent>) {
